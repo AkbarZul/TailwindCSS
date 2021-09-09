6cfa91300ffffe2f3c0aeef06b481ac22cb112aa
AkbarZul <akbar.zul73@gmail.com>
2021-09-09 15:06:29 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
 import './App.css';
-
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     <h1 className="text-red-500">yooo</h1>
-    </div>
+    <>
+     <Navbar />
+    </>
   );
 }
 
